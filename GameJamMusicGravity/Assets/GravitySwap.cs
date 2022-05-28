e51354c440237db4a91e77bb3202f23ba7c06846
Charlie Wensink <cpwensink@gmail.com>
2022-05-28 12:54:42 -0400

    Make gravity swap animation

--- GameJamMusicGravity/Assets/GravitySwap.cs
+++ GameJamMusicGravity/Assets/GravitySwap.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GravitySwap : MonoBehaviour
 {
     public float gravitySize = 80f;
+    public Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
@@ -14,25 +15,30 @@ public class GravitySwap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Gravity Swap", false);
         Vector2 CurrentDirection = Physics2D.gravity; //new Vector2(0f, 0f);
         if (Input.GetKey(KeyCode.A))
         {
             CurrentDirection = new Vector2(-gravitySize, 0f);
+            animator.SetBool("Gravity Swap", true);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             CurrentDirection = new Vector2(gravitySize, 0f);
+            animator.SetBool("Gravity Swap", true);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             CurrentDirection = new Vector2(0f, gravitySize);
+            animator.SetBool("Gravity Swap", true);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             CurrentDirection = new Vector2(0f, -gravitySize);
+            animator.SetBool("Gravity Swap", true);
         }
 
         Physics2D.gravity = CurrentDirection;
