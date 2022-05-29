64101e9cafd8ee32a57a7797e08e7b6ecca0b671
Charlie Wensink <cpwensink@gmail.com>
2022-05-29 09:43:33 -0400

    Make goal for the player to fall into at the end of each level

---
+++ GameJamMusicGravity/Assets/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject player;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Goal!");
+            Destroy(player);
+        }
+    }
+}
