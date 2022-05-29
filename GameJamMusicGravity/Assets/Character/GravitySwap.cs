a8250a69be8b2e933239461864f2a34ad952c018
Christian Hinkle <c4hinkle@gmail.com>
2022-05-29 16:45:33 -0400

    Have GravitySwap search for main camera by tag and give the character prefab's camera the MainCamera tag

--- GameJamMusicGravity/Assets/Character/GravitySwap.cs
+++ GameJamMusicGravity/Assets/Character/GravitySwap.cs
@@ -4,46 +4,53 @@ using UnityEngine;
 
 public class GravitySwap : MonoBehaviour
 {
+    /** Fall speed of the world */
     public float gravitySize = 80f;
-    public Animator playerAnimator;
-    public Animator cameraAnimator;
-    // Start is called before the first frame update
+    /** Reference to the character's animator */
+    public Animator characterAnimator;
+
+    // Cached reference to the level's main camera
+    private Animator cameraAnimator;
+
+
+
     void Awake()
     {
+        cameraAnimator = GameObject.FindWithTag("MainCamera").GetComponent<Animator>();
+
         Physics2D.gravity = new Vector2(0f, 0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        playerAnimator.SetBool("Gravity Swap", false);
+        characterAnimator.SetBool("Gravity Swap", false);
         cameraAnimator.SetBool("Gravity Swap", false);
         Vector2 CurrentDirection = Physics2D.gravity; //new Vector2(0f, 0f);
         if (Input.GetKey(KeyCode.A))
         {
             CurrentDirection = new Vector2(-gravitySize, 0f);
-            playerAnimator.SetBool("Gravity Swap", true);
+            characterAnimator.SetBool("Gravity Swap", true);
             cameraAnimator.Play("Camera_Grav", 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             CurrentDirection = new Vector2(gravitySize, 0f);
-            playerAnimator.SetBool("Gravity Swap", true);
+            characterAnimator.SetBool("Gravity Swap", true);
             cameraAnimator.Play("Camera_Grav", 0, 0);
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             CurrentDirection = new Vector2(0f, gravitySize);
-            playerAnimator.SetBool("Gravity Swap", true);
+            characterAnimator.SetBool("Gravity Swap", true);
             cameraAnimator.Play("Camera_Grav", 0, 0);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             CurrentDirection = new Vector2(0f, -gravitySize);
-            playerAnimator.SetBool("Gravity Swap", true);
+            characterAnimator.SetBool("Gravity Swap", true);
             cameraAnimator.Play("Camera_Grav", 0, 0);
         }
 
