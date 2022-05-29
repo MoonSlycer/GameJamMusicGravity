e5708f142465ca7b711acc87e8b91ec1f342c19d
Christian Hinkle <c4hinkle@gmail.com>
2022-05-29 18:09:32 -0400

    Make Instrument cs play crashAudioClip on collision enter and fill out Accordion prefab

--- GameJamMusicGravity/Assets/GameObjects/Instrument/Instrument.cs
+++ GameJamMusicGravity/Assets/GameObjects/Instrument/Instrument.cs
@@ -7,13 +7,21 @@ public class Instrument : MonoBehaviour
     /** Instrument crashing sound effect */
     public AudioClip crashAudioClip;
 
+    // Cached reference to our audio source to play through
+    private AudioSource audioSource;
+
     void Awake()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioSource.PlayOneShot(crashAudioClip);
     }
 }
