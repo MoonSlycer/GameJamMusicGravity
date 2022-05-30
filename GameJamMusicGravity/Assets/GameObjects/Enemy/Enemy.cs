a326c070c6a512ff542156159cb20bcd28d1f70b
Christian Hinkle <c4hinkle@gmail.com>
2022-05-29 23:51:09 -0400

    add random instrument force direction on death

--- GameJamMusicGravity/Assets/GameObjects/Enemy/Enemy.cs
+++ GameJamMusicGravity/Assets/GameObjects/Enemy/Enemy.cs
@@ -8,12 +8,22 @@ public class Enemy : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        GameObject[] instruments = GameObject.FindGameObjectsWithTag("Instrument");
+
         if(other.gameObject.tag == "Player")
         {
             Destroy(other.gameObject);
 
             GameObject gameMusic = GameObject.FindGameObjectWithTag("MusicPlayer");
             Destroy(gameMusic);
+
+            foreach (GameObject instrument in instruments)
+            {
+                Rigidbody2D rigidbody = instrument.GetComponent<Rigidbody2D>();
+
+                Vector2 randomDir = (new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f))).normalized;
+                rigidbody.AddForce(randomDir * 1000f);
+            }
         }
 
 
