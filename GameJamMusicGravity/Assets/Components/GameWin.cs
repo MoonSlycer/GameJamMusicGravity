b79a5a584b36377e480af985c96684f5f0167596
Christian Hinkle <c4hinkle@gmail.com>
2022-05-30 16:22:03 -0400

    Have GameWin scene turn down the GameMusic and play some room drums

--- GameJamMusicGravity/Assets/Components/GameWin.cs
+++ GameJamMusicGravity/Assets/Components/GameWin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameWon : MonoBehaviour
+public class GameWin : MonoBehaviour
 {
     void Start()
     {
