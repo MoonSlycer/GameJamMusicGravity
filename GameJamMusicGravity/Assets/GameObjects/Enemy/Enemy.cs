349b8e0237b882dd16164b9890f3808943ec896b
Brian2524 <b2hinkle@gmail.com>
2022-05-29 23:08:05 -0400

    destroy audio playwer when die

--- GameJamMusicGravity/Assets/GameObjects/Enemy/Enemy.cs
+++ GameJamMusicGravity/Assets/GameObjects/Enemy/Enemy.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Enemy : MonoBehaviour
 {
+
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.tag == "Player")
         {
             Destroy(other.gameObject);
         }
+
+        GameObject gameMusic = GameObject.FindGameObjectWithTag("MusicPlayer");
+
+        Destroy(gameMusic);
+
+
+        //GameMusic gameMusicScript = gameMusic.GetComponent<GameMusic>();
+
+        //gameMusicScript.StopAllTracks();
     }
 }
